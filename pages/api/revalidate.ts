ef67690aa617f2e182e0264357c5436c46613b4e
TJ <tj@TJs-MacBook-Pro.local>
2023-03-01 15:32:37 -0600

    undoing all of my changes to see if i still get the same error message

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -35,9 +35,6 @@ export default async function revalidate(
       req,
       process.env.SANITY_REVALIDATE_SECRET
     )
-
-    console.log("revalidate triggered: " + body);
-
     if (isValidSignature === false) {
       const message = 'Invalid signature'
       console.log(message)
@@ -51,28 +48,23 @@ export default async function revalidate(
     }
 
     const staleRoutes = await queryStaleRoutes(body as any)
-    console.log("staleRoutes: ", staleRoutes);
-
     await Promise.all(staleRoutes.map((route) => res.revalidate(route)))
 
     const updatedRoutes = `Updated routes: ${staleRoutes.join(', ')}`
     console.log(updatedRoutes)
     return res.status(200).send(updatedRoutes)
-
   } catch (err) {
     console.error(err)
     return res.status(500).send(err.message)
   }
 }
 
-type StaleRoute = '/' | `/blog/${string}` | '/features'
+type StaleRoute = '/' | `/blog/${string}`
 
 async function queryStaleRoutes(
   body: Pick<ParseBody['body'], '_type' | '_id' | 'date' | 'slug'>
 ): Promise<StaleRoute[]> {
   const client = createClient({ projectId, dataset, apiVersion, useCdn: false })
-
-  console.log("queryStaleRoutes: ", body, body._type)
 
   // Handle possible deletions
   if (body._type === 'post') {
@@ -97,19 +89,13 @@ async function queryStaleRoutes(
     }
   }
 
-  // create new if (body._type === 'features') to check for deletions
-
   switch (body._type) {
     case 'author':
       return await queryStaleAuthorRoutes(client, body._id)
     case 'post':
       return await queryStalePostRoutes(client, body._id)
-    
     case 'settings':
       return await queryAllRoutes(client)
-
-    case 'features':
-      return await queryStaleFeatureRoutes(client)
     default:
       throw new TypeError(`Unknown type: ${body._type}`)
   }
@@ -171,26 +157,4 @@ async function queryStalePostRoutes(
   slugs = await mergeWithMoreStories(client, slugs)
 
   return ['/', ...slugs.map((slug) => `/blog/${slug}`)]
-}
-
-const featureFields = groq`
-  _id,
-  title,
-  description,
-  coverImage
-`
-
-async function queryStaleFeatureRoutes(
-  client: SanityClient
-): Promise<StaleRoute[]> {
-
-  const response = await client.fetch(groq`
-    *[_type == "features"] | order(orderRank) {
-      ${featureFields}
-    }`
-  )
-
-  console.log("queryStaleFeatureRoutes triggered: ", response);
-
-  return response
 }

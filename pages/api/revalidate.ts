105e797e3b9c89a327a754583abe4a031e0a3726
TJ <tj@TJs-MacBook-Pro.local>
2023-03-02 09:56:45 -0600

    the / link breaks revalidation. removed it again. added exporst const revalidate 180 to home page only

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -54,8 +54,7 @@ export default async function revalidate(
     console.log("staleRoutes: ", staleRoutes);
 
     // remove '/' link from staleRoutes list if it exists because it breaks the res.revalidate function below
-      // testing to see if just removing the link from all but posts works
-    if(staleRoutes.includes('/') && body._type !== 'post') {
+    if(staleRoutes.includes('/')) {
       staleRoutes.splice(staleRoutes.indexOf('/'), 1)
     }
     console.log("staleRoutes after slice: ", staleRoutes);
@@ -137,7 +136,7 @@ async function _queryAllRoutes(client: SanityClient): Promise<string[]> {
 async function queryAllRoutes(client: SanityClient): Promise<StaleRoute[]> {
   const slugs = await _queryAllRoutes(client)
 
-  return ['/', '/blog', ...slugs.map((slug) => `/blog/${slug}` as StaleRoute)]
+  return ['/blog', ...slugs.map((slug) => `/blog/${slug}` as StaleRoute)]
 }
 
 async function mergeWithMoreStories(
@@ -168,7 +167,7 @@ async function queryStaleAuthorRoutes(
 
   if (slugs.length > 0) {
     slugs = await mergeWithMoreStories(client, slugs)
-    return ['/', '/blog', ...slugs.map((slug) => `/blog/${slug}`)]
+    return ['/blog', ...slugs.map((slug) => `/blog/${slug}`)]
   }
 
   return []
@@ -185,7 +184,7 @@ async function queryStalePostRoutes(
 
   slugs = await mergeWithMoreStories(client, slugs)
 
-  return ['/', '/blog', ...slugs.map((slug) => `/blog/${slug}`)]
+  return ['/blog', ...slugs.map((slug) => `/blog/${slug}`)]
 }
 
 async function queryStaleFeatureRoutes(

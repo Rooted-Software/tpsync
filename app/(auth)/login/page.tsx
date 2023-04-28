c451e6b2554aa282a4d12825a01fd7813349f504
Marshal Morse <marshalmorse@gmail.com>
2023-04-27 22:32:01 -0500

    bugfix-duplicate-link

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next"
 import Link from "next/link"
-import Link from 'next/link'
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"

327e4c23f0c32e48bc3eaa6eb6171e83e2bcc476
Marshal Morse <marshalmorse@gmail.com>
2023-07-18 16:52:17 -0500

    wip-fix-login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -25,8 +25,9 @@ export default function LoginPage() {
           Back
         </>
       </Link>
-      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-        <div className="flex flex-col space-y-2 text-center">
+      <div className="bg-dark text-white lg:p-8 ">
+        <div className="m-auto flex w-full flex-col justify-center space-y-6 ">
+          <div className="flex flex-col space-y-2 text-center  ">
           <Icons.logo className="mx-auto h-6 w-6" />
           <h1 className="text-2xl font-semibold tracking-tight">
             Welcome back
@@ -45,6 +46,7 @@ export default function LoginPage() {
           </Link>
         </p>
       </div>
+    </div>
     </div>
   )
 }

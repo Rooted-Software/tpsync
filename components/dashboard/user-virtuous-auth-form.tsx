d5e4745b99e0dbe2ec61526d8af4d5ff4a5555c3
Marshal Morse <marshalmorse@gmail.com>
2023-07-19 15:51:05 -0500

    fix-password-text-color

--- components/dashboard/user-virtuous-auth-form.tsx
+++ components/dashboard/user-virtuous-auth-form.tsx
@@ -98,7 +98,7 @@ export function UserVirtuousAuthForm({
             <input
               id="password"
               placeholder="xyazb1Cfna"
-              className="my-0 mb-2 block h-9 w-full rounded-full border border-slate-300 py-2 px-3 text-sm placeholder:text-slate-400 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
+              className="my-0 mb-2 block h-9 w-full rounded-full border border-slate-300 py-2 px-3 text-sm text-slate-500 placeholder:text-slate-500 hover:border-slate-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1"
               type="password"
               autoCapitalize="none"
               autoCorrect="off"

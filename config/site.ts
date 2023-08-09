1548d8aafd7a34ba16beb8d069b5c0b11e8eda9e
Marshal Morse <marshalmorse@gmail.com>
2023-08-09 16:37:53 -0500

    fix-site-config

--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
   name: 'DonorSync',
   description:
     'An open source application built using the new router, server components and everything new in Next.js 13.',
-  url: 'https://donorsync.org',
+  url: 'https://app.donorsync.org',
   ogImage: 'https://app.donorsync.org/og.jpg',
   links: {
     twitter: 'https://twitter.com/RootedSoftware',

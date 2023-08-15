e4f5be87d5c907fc79d5cb344f0e7360cbb87808
Marshal Morse <marshalmorse@gmail.com>
2023-08-15 12:58:56 -0500

    wip-hmac-security

--- app/api/webhooks/v1/anedot/route.ts
+++ app/api/webhooks/v1/anedot/route.ts
@@ -1,17 +1,17 @@
 import { db } from '@/lib/db'
 import { headers } from 'next/headers'
 import crypto from 'crypto'
-
+import { buffer } from "micro";
 
 export async function POST(req: Request) {
   const SECRET_KEY=process.env.ANEDOT_WEBHOOK_SECRET || ''
-  const body = await req.text()
+  const reqBuffer = req.body
   console.log('in webhook req post')
   const signature = headers().get('X-Request-Signature') as string
  
   //attempt whole body...not just text
   const hmac = crypto.createHmac('sha256', SECRET_KEY);
-  hmac.update(body);
+  hmac.update(reqBuffer);
   const hmacDigest = hmac.digest('hex');
   console.log(hmacDigest)
   console.log(signature)
